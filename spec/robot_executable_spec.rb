2dcfdb9aff9d270399637c5cb9c8d6c3ac5e92c9
Dmitry Maksyoma <ledestin@gmail.com>
2014-03-23 10:12:49 +1300

    * Added top-level test for the executable command.

---
+++ spec/robot_executable_spec.rb
@@ -0,0 +1,16 @@
+require 'open3'
+
+describe 'robot executable' do
+  it 'processes input commands and prints output' do
+    input = <<-EOF
+      PLACE 0,0,NORTH
+      MOVE
+      REPORT
+    EOF
+    output = <<-EOF
+      0,1,NORTH
+    EOF
+
+    expect(Open3.capture2e('./robot', input)).to eq output
+  end
+end
