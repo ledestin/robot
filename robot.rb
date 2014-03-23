9cb0d12c474fdde7b8725e5abe5463554aa71525
Dmitry Maksyoma <ledestin@gmail.com>
2014-03-23 19:40:37 +1300

    * Refactor. Added parse_place_command method.

--- robot.rb
+++ robot.rb
@@ -5,19 +5,25 @@ class Robot
 
   def execute_command command
     command.strip!
-    if command =~ /^PLACE\s+(.+)$/
-      x, y, direction = $1.split /\s*,\s*/
-      begin
-	x, y = Integer(x), Integer(y)
-      rescue ArgumentError
-	return
-      end
-      return unless direction =~ /^(#{DIRECTIONS.join('|')})$/
+    if command =~ /^PLACE\s+/
+      x, y, direction = parse_place_command command
+      return unless x
       return if x > TABLE_WIDTH
 
       @coords = [x, y, direction].join(',')
       return
     end
     @coords if command == 'REPORT'
+  end
+
+  def parse_place_command str
+    x, y, direction = str.split(/\s+/, 2).last.split /\s*,\s*/
+    begin
+      x, y = Integer(x), Integer(y)
+    rescue ArgumentError
+      return
+    end
+    return unless direction =~ /^(#{DIRECTIONS.join('|')})$/
+    [x, y, direction]
   end
 end
