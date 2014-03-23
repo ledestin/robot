e3969db192b358ec3b6c1a0eb197d059d1fa60c6
Dmitry Maksyoma <ledestin@gmail.com>
2014-03-23 16:04:43 +1300

    * Refactor tests.

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -16,18 +16,20 @@ EOF
 end
 
 describe Robot do
-  it 'command PLACE 0,0,NORTH places robot' do
-    subject.execute_command 'PLACE 0,0,NORTH'
-    expect(subject.execute_command('REPORT')).to eq '0,0,NORTH'
-  end
+  context 'command PLACE' do
+    it '0,0,NORTH places robot' do
+      subject.execute_command 'PLACE 0,0,NORTH'
+      expect(subject.execute_command('REPORT')).to eq '0,0,NORTH'
+    end
 
-  it 'command PLACE a,0,NORTH is ignored' do
-    subject.execute_command 'PLACE a,0,NORTH'
-    expect(subject.execute_command('REPORT')).to be_nil
-  end
+    it 'a,0,NORTH is ignored' do
+      subject.execute_command 'PLACE a,0,NORTH'
+      expect(subject.execute_command('REPORT')).to be_nil
+    end
 
-  it 'command PLACE 0,a,NORTH is ignored' do
-    subject.execute_command 'PLACE 0,a,NORTH'
-    expect(subject.execute_command('REPORT')).to be_nil
+    it '0,a,NORTH is ignored' do
+      subject.execute_command 'PLACE 0,a,NORTH'
+      expect(subject.execute_command('REPORT')).to be_nil
+    end
   end
 end
