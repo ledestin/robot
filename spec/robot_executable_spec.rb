fa055b35f8d9a15e3013368395306aae5cb8c3c2
Dmitry Maksyoma <ledestin@gmail.com>
2014-04-02 03:39:11 +1300

    * Better test comment.

--- spec/robot_executable_spec.rb
+++ spec/robot_executable_spec.rb
@@ -27,7 +27,7 @@ EOF
     expect(execute_commands input).to eq output
   end
 
-  xit 'ignores all moving commands after it was placed outside of table' do
+  xit 'report prints nothing if robot is not on the table' do
     output = execute_commands <<-EOF
       PLACE -1,0,NORTH
       REPORT
