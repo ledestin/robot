893aefaf27baedb3a265799811dcb71bb00278be
Dmitry Maksyoma <ledestin@gmail.com>
2014-04-02 03:50:46 +1300

    * Refactor. Better test comment.

--- spec/robot_executable_spec.rb
+++ spec/robot_executable_spec.rb
@@ -27,7 +27,7 @@ EOF
     expect(execute_commands input).to eq output
   end
 
-  it 'report prints nothing if robot is not on the table' do
+  it 'REPORT prints nothing if robot is not on the table' do
     output = execute_commands <<-EOF
       PLACE -1,0,NORTH
       REPORT
