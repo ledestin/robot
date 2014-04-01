10e93f48d10b7284adfa4094f94cce25d0fab395
Dmitry Maksyoma <ledestin@gmail.com>
2014-04-02 03:58:34 +1300

    * Added tests for Robot::State.

---
+++ spec/state_spec.rb
@@ -0,0 +1,8 @@
+require './robot'
+
+describe Robot::State do
+  it 'handles conversion to String' do
+    state = Robot::State.new 0, 0, 'NORTH'
+    expect(state.to_s).to eq '0,0,NORTH'
+  end
+end
